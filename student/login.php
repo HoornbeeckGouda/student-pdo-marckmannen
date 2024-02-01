a21ca629cfc21003924f9465dfd4acaccd0e5d9b
Epixness <karsten@kolibrilogistiek.nl>
2024-02-01 19:41:19 +0100

    inloggen werkt nu

--- student/login.php
+++ student/login.php
@@ -2,16 +2,15 @@
 include 'inc/header.php';
 ?>
 <div class="achterinlog">
-<div class="inlogformulier">
-<h1>INLOGGEN</h1>
-    <form method="POST">
-        <input type="text" name="inloggen" size="25" class="gebruikersnaam" placeholder="Gebruikersnaam"><br>
-        <input type="password" name="password" size="25" class="wachtwoord" placeholder="Wachtwoord"><br>
-        <input type="submit" name="submit" class="loggin" value="Log In">
-
-    </form>
-</div>
+    <div class="inlogformulier">
+        <h1>INLOGGEN</h1>
+            <form method="POST">
+                <input type="text" name="inloggen" size="25" class="gebruikersnaam" placeholder="Gebruikersnaam"><br>
+                <input type="password" name="password" size="25" class="wachtwoord" placeholder="Wachtwoord"><br>
+                <input type="submit" name="submit" class="loggin" value="Log In">
+            </form>
     </div>
+</div>
  <?php
 // inlogvariables toekennen
 if (isset($_POST['submit'])) {
@@ -19,27 +18,44 @@ if (isset($_POST['submit'])) {
     $wachtwoord = $_POST['password'];
 
     // checken of inloggegevens kloppen
-    $query1 = "SELECT id, gebruikersnaam, wachtwoord, functie_id FROM gebruiker WHERE gebruikersnaam = :?; AND wachtwoord = :?";
+    $query1 = "SELECT id, gebruikersnaam, wachtwoord, id FROM gebruiker WHERE gebruikersnaam = :user AND wachtwoord = :ww";
     $result=$dbconn->prepare($query1);
 
-    $stmt = mysqli_prepare($conn, $query1);
-    mysqli_stmt_bind_param($stmt, "ss", $inlognaam, $wachtwoord);
-    mysqli_stmt_execute($stmt);
-    $uitvoer = mysqli_stmt_get_result($stmt);
+    $result->bindParam(':ww',$ww);
+    $ww=$wachtwoord;
 
-    //checken of user bestaat
-    $aantal = mysqli_num_rows($uitvoer);
-    if ($aantal == 1) {
-        // de data fetchen van de uitgevoerde query
-        $row = mysqli_fetch_assoc($uitvoer);
-        $_SESSION['gebruikersnaam'] = $row['gebruikersnaam'];
-        $_SESSION['functie_id'] = $row['functie_id'];
-        $_SESSION['user_id'] = $row['id'];
-        header("refresh:0, url=./homepage.php");
-    } else {
-        echo "<h2 class='foutegegevens'>Helaas! Foute gegevens</h2>";
-        session_unset();
+    $result->bindParam(':user',$user);
+    $user=$inlognaam;
+
+
+    try {
+
+        $result->execute();
+
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+
+        //checken of user bestaat
+        if ($result->rowCount()==1) {
+
+            $_SESSION['gebruikersnaam'] = $result->fetchColumn(2);
+            $_SESSION['user_id'] = $result->fetchColumn(1);
+            header("refresh:0, url=./studenten.php");
+        }
+         else {
+            echo "<script>"<h2 class='foutegegevens'>Helaas! Foute gegevens</h2>"</script>";
+            session_unset();
+        }
     }
+
+    catch (PDOException $e) {
+
+        echo "foutje: " . $e->getMessage();
+
+        echo "<script>alert('klanten niet gevonden');</script>";
+
+    }
+
+
 }
 
 include 'inc/footer.php';
